1b8322c7396c9e53213bf0458cbc5d16abbf6c97
Jordy IJsselstijn <jordyijsselstijn@gmail.com>
2019-04-16 00:07:20 +0200

    Forgot to commit this work.

--- SchuldEisers.js
+++ SchuldEisers.js
@@ -7,6 +7,23 @@ module.exports = {
         address_1: '',
         address_2: '',
         city: ''
-
+    },
+    duo: {
+        image: 'https://cstories.nl/wp-content/uploads/2017/12/LOG-DUO.jpg',
+        type: 'Overheids Instelling',
+        phone_number: '0505 99 99 99',
+        email_address: '',
+        address_1: 'Kempkensberg 12',
+        address_2: '9722 TB',
+        city: 'Groningen'
+    },
+    wehkamp: {
+        image: 'https://www.samr.nl/wp-content/uploads/2017/07/wehkamp.jpg',
+        type: 'Online Winkel',
+        phone_number: '0388454545',
+        email_address: 'klantenservice@wehkamp.nl',
+        address_1: '',
+        address_2: '',
+        city: ''
     }
 };
